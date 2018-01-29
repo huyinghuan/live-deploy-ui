082f86efed0ccf925f8c3623398f3377afdecb17
hyh <ec.huyinghuan@gmail.com>
2018-01-29 18:23:02 +0800

    add location more setting

--- src/app/components/location-edit.ts
+++ src/app/components/location-edit.ts
@@ -13,11 +13,23 @@ let template:string =
     <input type="number" placeholder="可选，默认0秒为nginx默认值" [(ngModel)]="location.timeout">
   </div>
   <div class="field">
+    <a *ngIf="!more" (click)="more=true">高级</a>
+    <a *ngIf="more" (click)="more=false">收起</a>
+  </div>
+  <div class="field">
     <div class="ui right floated buttons">
       <button class="ui green button" (click)="SaveLoction()">保存</button>
       <div class="or"></div>
       <button class="ui gray button" (click)="CancelEditLocation()">取消</button>
     </div>
+  </div>
+</div>
+<div class="inline fields" *ngIf="more">
+  <div class="fourteen wide field">
+    <textarea width="100%"  [(ngModel)]="location.more" ></textarea>
+  </div>
+  <div class="two wide field">
+    <button class="ui blue button" (click)="SaveLoction()">保存</button>
   </div>
 </div>
 </div>
@@ -32,6 +44,7 @@ export class LocationConfigEditComponent {
   @Input() public location:any;
   public onCancelEditLocation = new EventEmitter()
   public onSaveLocation = new EventEmitter()
+  private more:boolean = false
   constructor(){}
   SaveLoction(){
     this.onSaveLocation.emit(this.location)
