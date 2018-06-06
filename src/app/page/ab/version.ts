d34f8c90f9cbe33a6e11ab2026b489aeb7c516e8
hyh <ec.huyinghuan@gmail.com>
2018-06-06 11:18:38 +0800

    update ui

--- src/app/page/ab/version.ts
+++ src/app/page/ab/version.ts
@@ -33,7 +33,7 @@ let template:string =
       <div class="field">
         <button class="ui green icon button" (click)="save()"><i class="save icon"></i>保存</button>
 
-        <button  *ngIf="ab.status == 0" class="ui blue icon button" (click)="deploy()"><i class="paper plane icon"></i>部署</button>
+        <button class="ui blue icon button" (click)="deploy()"><i class="paper plane icon"></i>部署</button>
         <button  *ngIf="ab.status == 1" class="ui red icon button" (click)="unDeploy()"><i class="stop icon"></i>禁用</button>
         <button  *ngIf="ab.status == -1" class="ui red icon button" (click)="deploy()"><i class="stop icon"></i>部署中...</button>
       </div>
